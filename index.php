bfb371a717c882a6d055be7110abcaf300231df2
JanSlechta360 <60642873+JanSlechta360@users.noreply.github.com>
2020-06-04 13:45:05 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+      <body>
+      
+      
+      
+      <h2>Priklad c.5 - domaci ukol - schody</h2>
+<?php
+    for ($p=0; $p <= 400; $p+= 20) { 
+?>  <div style="display: block; width:<?= $p ?>px; height: 20px; background-color: #454d59;"></div> 
+<?php
+}
+
+?>
+
+      </body>
+</html>
+
+
+
+
